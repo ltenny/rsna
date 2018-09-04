e265e65651dca44c83dd40b143d03b14f7234c26
Larry Tenny <ltenny@gmail.com>
2018-09-04 14:39:10 -0500

    added oringinals with RoI boxes

--- Prepare/cxrimage.py
+++ Prepare/cxrimage.py
@@ -17,10 +17,29 @@ def extract_image(image_data, box):
     return image_data[y1:y2,x1:x2]
 
 # writes the extracted bounding box to the given output path with new filename
-def write_image(image_data, path):
+def write_image(image_data, path, filename=None):
     im = Image.fromarray(image_data)
-    im.save(os.path.join(path,'%s.jpg' % str(uuid.uuid4())))
-    
+    if filename:
+        im.save(os.path.join(path, filename))
+    else:
+        im.save(os.path.join(path,'%s.jpg' % str(uuid.uuid4())))
+
+def write_image_with_bounding_boxes(image_data, path, filename, boxes, rgb=[128,0,0]):
+    for box in boxes:
+        image_data = _overlay_box(image_data, box, rgb)
+    im = Image.fromarray(image_data)
+    im.save(os.path.join(path, filename))
+
+def _overlay_box(im, box, rgb, stroke=6):
+    y1,x1,height, width = box
+    y2 = y1 + height
+    x2 = x1 + width
+    im[y1:y1+stroke, x1:x2] = rgb
+    im[y2:y2+stroke, x1:x2] = rgb
+    im[y1:y2, x1:x1+stroke] = rgb
+    im[y1:y2, x2:x2+stroke] = rgb
+    return im    
+
 # gets the image data from the dicom file
 def get_image_data(filename, path):
     fullpath = os.path.join(path, filename)
--- Prepare/main.py
+++ Prepare/main.py
@@ -4,10 +4,12 @@
 #
 
 import tensorflow as tf
+import numpy as np
 from labelrecord import LabelRecord
 import cxrimage
 
 FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('originals_dir','originals',"""Directory for the original cxr images""")
 tf.app.flags.DEFINE_string('positives_dir','positives',"""Directory for the positive image regions""")
 tf.app.flags.DEFINE_string('negatives_dir','negatives',"""Directory for the negative image regions""")
 tf.app.flags.DEFINE_string('label_file','../input/stage_1_train_labels.csv',"""Path to labels .csv file""")
@@ -21,6 +23,11 @@ def _init_output_directories():
     if tf.gfile.Exists(FLAGS.negatives_dir):
         tf.gfile.DeleteRecursively(FLAGS.negatives_dir)
     tf.gfile.MakeDirs(FLAGS.negatives_dir)
+
+    if tf.gfile.Exists(FLAGS.originals_dir):
+        tf.gfile.DeleteRecursively(FLAGS.originals_dir)
+    tf.gfile.MakeDirs(FLAGS.originals_dir)
+
 
 # returns collection of all bounding boxes
 def _get_bounding_boxes(records):
@@ -37,11 +44,16 @@ def main(argv=None):
     all_bounding_boxes = _get_bounding_boxes(label_records)
 
     for (_,v) in label_records.items():
+        print('processing %s' % v.filename)
         image = cxrimage.get_image_data(v.filename, FLAGS.image_path)
         if v.hasBoundingBox:
             for box in v.boundingBoxes:
-                print('writing image for %s' % v.filename)
                 cxrimage.write_image(cxrimage.extract_image(image, box),FLAGS.positives_dir)
+        else:
+            box = all_bounding_boxes[np.random.random_integers(0, len(all_bounding_boxes) - 1)]
+            cxrimage.write_image(cxrimage.extract_image(image, box), FLAGS.negatives_dir)
+
+        cxrimage.write_image_with_bounding_boxes(image, FLAGS.originals_dir, "%s.jpg" % v.filename, v.boundingBoxes)
 
 if __name__ == '__main__':
     tf.app.run()
