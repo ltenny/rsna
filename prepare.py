51266228628e8cc9790d11a65972bb61b646c920
Larry Tenny <ltenny@gmail.com>
2018-09-14 09:17:43 -0500

    added tfrecord support

--- prepare.py
+++ prepare.py
@@ -10,6 +10,7 @@ from utils.cxrimage import CXRImage
 from utils.labelrecord import LabelRecord
 from utils.box import Box
 import os
+import tensorflow as tf 
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('examples_dir','examples',"""Directory for the cxr example images""")
@@ -40,6 +41,35 @@ def _init_output_directories():
         tf.gfile.DeleteRecursively(FLAGS.examples_dir)
     tf.gfile.MakeDirs(FLAGS.examples_dir)
 
+def _bytes_list_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _int64_list_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))    
+
+def _create_tf_records(records):
+    cnt = 0
+    writer = tf.python_io.TFRecordWriter(os.path.join(FLAGS.examples_dir,'examples.txt'))
+    for (_,v) in records.items():
+        if v.hasBoundingBox:
+            image = CXRImage.get_image_data(v.filename, FLAGS.image_path)
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'image': _bytes_list_feature(image.flatten().tobytes()),
+                'image/width': _int64_feature(image.shape[0]),
+                'image/height': _int64_feature(image.shape[1]),
+                'image/channels': _int64_feature(image.shape[2]),
+                'image/format': _bytes_list_feature('jpg'.encode('utf8')),
+                'filename' : _bytes_list_feature((os.path.splitext(v.filename)[0]).encode('utf8')),
+                'ground_true_boxes': _int64_list_feature(v.boundingBoxes.flatten())
+            }))
+            cnt = cnt + 1
+            print('writing record %d    ' % cnt, end="\r")
+            writer.write(example.SerializeToString())
+    writer.close()
+
 # main entry point
 def main(argv=None):
     _init_output_directories()
@@ -49,6 +79,7 @@ def main(argv=None):
     label_records = lr.load(FLAGS.label_file)
 
     all_bounding_boxes = Box.get_all_bounding_boxes(label_records)
+    _create_tf_records(label_records)
 
     for (_,v) in label_records.items():
         print('processing %s' % v.filename)
@@ -58,7 +89,7 @@ def main(argv=None):
             for i in range(0,v.boundingBoxes.shape[0]):
                 box = v.boundingBoxes[i,:]
                 CXRImage.write_image(CXRImage.extract_image(image, box),FLAGS.positives_dir)
-            CXRImage.write_image(image, FLAGS.examples_dir, "%s.jpg" % basefilename)
+            #CXRImage.write_image(image, FLAGS.examples_dir, "%s.jpg" % basefilename)
         else:
             i = np.int32(np.random.randint(0, all_bounding_boxes.shape[0] - 1))
             box = all_bounding_boxes[i,:]
